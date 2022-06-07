e63841c3217ceab0fa8e5b0a2fa33a16a1cadf96
beriem <84970894+beriem@users.noreply.github.com>
2022-06-07 10:33:46 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,9 +46,9 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "1d225992f92466b4b98eb7ad8708f1d1",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"index.html": "566ee9e1497a2990f88bb9f069dc7809",
-"/": "566ee9e1497a2990f88bb9f069dc7809",
-"main.dart.js": "b549dc65cc88a6bc03f9ff816f13f6bc",
+"index.html": "6d6dc26f8e8f6501b8a5a8466cfd78e7",
+"/": "6d6dc26f8e8f6501b8a5a8466cfd78e7",
+"main.dart.js": "39ab436fc686a98105582d7bc0ad0ce0",
 "version.json": "b0b73e099387cd68b918db43c47384a1"
 };
 
